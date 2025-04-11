3b35019fe35522352aecb1248e629242baafb836
pascard20 <141923751+pascard20@users.noreply.github.com>
2025-04-11 21:08:06 +0200

    Optimise project
    - reduce main background image size for faster load time
    - add favicon
    - add npm scripts for improving development and build process
    - utilise Webpack production mode

---
+++ webpack.prod.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const htmlPath = "./src/template.html"
+const jsPath = "./src/main.js"
+
+module.exports = {
+  mode: "production",
+  entry: jsPath,
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: [htmlPath],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: htmlPath, }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin(),],
+  },
+};
